b4db09991612fad4f0ae141d643890476b638031
Dave Taylor <davidtaylor970@googlemail.com>
2021-10-19 16:59:19 +0100

    diy-kata

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,7 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+    const simpsons = ['Bart', 'Lisa','Maggie'];
 
+    return simpsons.slice(0, 2).join(', ') + ' & ' + simpsons.slice(-1);
+};
+ 
 module.exports = joinNames;
